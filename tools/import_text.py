73b356e4af49435a69594989334522dd70c105b6
cokkeijigen <cokkeijigen@qq.com>
2023-10-18 12:06:21 +0800

    update...

--- tools/import_text.py
+++ tools/import_text.py
@@ -1,5 +1,9 @@
 import os
 
+ipt_text: str = './doko_iku_text'
+org_text: str = './old/txt'
+out_text: str = './new'
+
 def file_read(path: str, encoding: str) -> list[str]:
     result: list[str]
     with open(path, 'r',  encoding=encoding) as f:
@@ -41,14 +45,11 @@ def text_parse(text_array: list[str]) -> list[list[str]]:
                 if len(text) > 0:
                     num = text[0].split('|')[1].strip()
                     text = text[1]
-                    # text = text.replace('＠', '仐')
-                    # text = text.replace('@', '仐')
-                    # text = text.replace('【', '亂')
-                    # text = text.replace('】', '亃')
-                    # text = text.replace('｛', '乷')
-                    # text = text.replace('｝', '乸')
+                    text = text.replace('｛', '乷')
+                    text = text.replace('｝', '乸')
+                    text = text.replace('@', '仐')
+                    text = text.replace('＠', '仐')
                     text = text.replace('「', '乽')
-                    text = text.replace('@', '＠')
                     text = text.replace('・', '·')
                     text = text.replace('♪', '§')
                     text = text.replace('〜', '～')
@@ -66,37 +67,33 @@ def text_com(cn_text: list[list[str]], old_text: list[str]) -> list[str]:
         text_list: list[list[int|str]] = block[1]
         for i in range(start, end + 1):
             cur_text: str = old_text[i]
-            if '#' in cur_text or '/' in cur_text or '％' in cur_text or '*' in cur_text or 'GOTO' in cur_text or '$' in cur_text: continue
+            if '#' in cur_text or '/' in cur_text or '％' in cur_text or '*' in cur_text or 'GOTO' in cur_text or '$' in cur_text or ('亂' in cur_text and '亃' in cur_text ): continue
             old_text[i] = None
-        text_map: dict[int, str] = {}
-        for text in text_list:
-            num  = text[0]
-            text = text[1]
-            t_val = text_map.get(num)
-            if t_val != None:
-                t_val += text
-            else:
-                t_val = text
-            text_map[num] = t_val
         offset: int = 0
-        for num, text in text_map.items():
-            if old_text[start - 1 + num + offset] != None:
+        for text in text_list:
+            tlen = int(text[0])
+            text = str(text[1])
+            if '【' in text and '】' in text:
+                continue
+            if old_text[start - 1 + offset] != None:
                 while True:
                     offset += 1
-                    if old_text[start - 1 + num + offset] == None:
+                    if old_text[start - 1 + offset] == None:
                         break
-            old_text[start - 1 + num + offset] = text;
+                pass
+            old_text[start - 1 + offset] = text.replace('\\n','\n');
+            offset += tlen
     result = [text for text in old_text if text != None]
     return result
 
 if __name__ == '__main__':
-    for f in os.listdir('./doko_iku_text/'):
-        old_text_array: list[str] = file_read(f'./old/txt/{f}', 'gbk')
-        new_text_array: list[str] = file_read(f'./doko_iku_text/{f}', 'utf-8')
+    for f in os.listdir(ipt_text):
+        old_text_array: list[str] = file_read(f'{org_text}/{f}', 'gbk')
+        new_text_array: list[str] = file_read(f'{ipt_text}/{f}', 'utf-8')
         parsed_text: list[list[str]]  = text_parse(new_text_array)
         result: list[str] = text_com(parsed_text, old_text_array)
         try:
-            file_write(f'./new/{f}', result, 'gbk')
+            file_write(f'{out_text}/{f}', result, 'gbk')
             print(f)
         except Exception as err:
             print(f"{f}: {str(err)}")
--- tools/text_getter.py
+++ tools/text_getter.py
@@ -1,5 +1,8 @@
 import os
 
+org_text: str = './old/txt'
+out_text: str = './text'
+
 def text_getter(text_array: list[str]) -> str:
     text_filter: str = ''
     for i in range(0, len(text_array)):
@@ -23,29 +26,33 @@ def text_getter(text_array: list[str]) -> str:
         end: str = lines[-1].split(':')[0]
         result += f"[block-start: {start}]#{i}\n"
         count: int = 1
+        save_line_text: str = ''
+        save_line_nums: int = 0
         for n in range(0, len(lines)):
             line: str = lines[n]
             if ":" not in line: continue
             line = line.split(":")[1]
-            if count < 10:
-                c_text = '00' + str(count)
-            elif count < 100:
-                c_text = '0' + str(count)
-            result += f'○○|{c_text}|○○|{line}\n'
-            result += f'●●|{c_text}|●●|{line}\n'
-            if(n != len(lines) - 1):
-                result += '\n'
+            save_line_text += line
+            save_line_nums += 1
+            if line[-1] == '＠' or line[-1] == '】':
+                result += f'○○|0{save_line_nums}|○○|{save_line_text}\n'
+                result += f'●●|0{save_line_nums}|●●|{save_line_text}\n'
+                if n != len(lines) -1: result += '\n'
+                save_line_nums = 0
+                save_line_text = ''
+            else:
+                save_line_text += '\\n'
             count += 1
         result += f"[block-end: {end}]#{i}\n\n"
         pass
     return result
 
 if __name__ == '__main__':
-    for f in os.listdir('./old/txt'):
-        fp = open(f'./old/txt/{f}', 'r', encoding='shift-jis')
+    for f in os.listdir(org_text):
+        fp = open(f'{org_text}/{f}', 'r', encoding='shift-jis')
         text_array: list[str] = fp.readlines()
         fp.close()
         text = text_getter(text_array)
-        fp = open(f'./text/{f}', 'w' ,encoding='utf-8')
+        fp = open(f'{out_text}/{f}', 'w' ,encoding='utf-8')
         fp.write(text)
         fp.close()
